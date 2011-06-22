59be47f51318d0abf19d873eadd8c8f676bed9b7
Benny Daon <bennydaon@gmail.com>
2011-06-22 14:51:27 +0300

    calling urls by name. just the index for now

--- src/map/map_info/urls.py
+++ src/map/map_info/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import *
 
 
+# TODO: add names to the urls, like with `map-home`
 urlpatterns = patterns('',
     (r'^point/view/(\d+)/$','map_info.views.view_detailed'),
     (r'^point/add_comment/(\d+)/$','map_info.views.add_comment'),
     (r'^add/$','map_info.views.add_point'),
     (r'^getfile/(?P<file>.*)/$','map_info.views.get_file'),
-    (r'^$', 'map_info.views.main'),
+    url(r'^$', 'map_info.views.main', name='map-home'),
 )
